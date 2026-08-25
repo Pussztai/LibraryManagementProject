39e5bd3fcb16a95aa8d1faa7f2883b4e3caaa9b3
PuszAtiCkik <pusztai.attila@gmail.hu>
2026-08-25 15:17:01 +0200

    Created Entities and added DbContext

--- LibraryManagement/Entities/Loan.cs
+++ LibraryManagement/Entities/Loan.cs
@@ -4,7 +4,6 @@
         public int BookId { get; set; }
 
         public int MemberId { get; set; }
-
         public DateTime BorrowedAt { get; set; }
 
         public DateTime? ReturnedAt { get; set; }
